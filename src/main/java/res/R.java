b3e5c11781b3ab618444e2841b49476e634a126d
asaran07 <saran.arsh07@icloud.com>
2024-01-08 10:36:30 -0800

    Add resource class with speed and visibility constants
    In this commit, a new resource class (R) has been introduced. This class houses two nested static classes, 'speed' and 'visibility', each containing various constant values used as speed and visibility settings respectively. These settings will be used across the project to standardize speed and visibility configurations.

--- src/main/java/res/R.java
+++ src/main/java/res/R.java
@@ -1,2 +1,17 @@
-package res;public class R {
+package res;
+
+/**
+ * Resource Class
+ */
+
+public class R {
+    public static class speed {
+        public static final double NORMAL = 0.5;
+        public static final double FAST = 0.3;
+        public static final double SLOW = 0.7;
+    }
+    public static class visibility {
+        public static final double INVISIBLE = 0.0;
+        public static final double VISIBLE = 1.0;
+    }
 }
