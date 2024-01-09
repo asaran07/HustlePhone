05b3eb075765f7343e80f0635eb1bd1baf6aeda8
asaran07 <saran.arsh07@icloud.com>
2024-01-08 21:13:37 -0800

    Refactor DialogFlowManager to handle new game state
    The DialogFlowManager has been refactored to handle game states better by introducing 'gameState' and 'isNewGame' variables. It also now includes methods that provide a game state message and options for the player. GameController is updated to use these new options and messages, and to set 'isNewGame' to true when a new game initializes. Resources for intro messages and player options were also added.

--- src/main/java/com/example/vom/GameController.java
+++ src/main/java/com/example/vom/GameController.java
@@ -30,11 +30,8 @@ public class GameController implements Initializable {
     @FXML private Label convoLabel;
     @FXML private Button replyButton1;
     @FXML private Button replyButton2;
-    @FXML private Pane mainAnchorPane;
-    @FXML private boolean checkForContinue;
 
-    private int dialogueCounter = 0;
-    private boolean introFinished = false;
+    private DialogFlowManager dialogFlowManager = new DialogFlowManager();
     private final Timeline introTimeline = new Timeline();
     private final Timeline introTextTimeline = new Timeline();
 
@@ -44,6 +41,7 @@ public class GameController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        dialogFlowManager.setNewGame(true);
         preparePanes();
         prepareButtons();
         initActions();
@@ -103,6 +101,12 @@ public class GameController implements Initializable {
             startFadeAnimation(titleScreenPane, true, R.speed.NORMAL);
             introTimeline.play();
         });
+        replyButton1.setOnAction(actionEvent -> {
+            dialogFlowManager.advanceState(replyButton1.getText());
+        });
+        replyButton2.setOnAction(actionEvent -> {
+            dialogFlowManager.advanceState(replyButton2.getText());
+        });
     }
 
     private void makeInvisible(final Node theNode) {
@@ -128,77 +132,143 @@ public class GameController implements Initializable {
         introTimeline.getKeyFrames().add(new KeyFrame(Duration.seconds(2), e -> playIntro()));
     }
 
-    public void startTypewriterEffect(final String theText) {
+    public void startTypewriterEffect(final String theText, Timeline theTimeline) {
         StringBuilder sb = new StringBuilder();
-        Timeline tl = new Timeline();
+        Timeline typewriterTimeline = new Timeline();
+        theTimeline.pause();
         for (int i = 0; i < theText.length(); i++) {
             char nextLetter = theText.charAt(i);
             KeyFrame kf = new KeyFrame(Duration.seconds(i * 0.07), e -> {
                 sb.append(nextLetter);
                 convoLabel.setText(sb.toString());
             });
-            tl.getKeyFrames().add(kf);
+            typewriterTimeline.getKeyFrames().add(kf);
         }
-        tl.play();
+        typewriterTimeline.setOnFinished(e -> theTimeline.play());
+        typewriterTimeline.play();
     }
 
     public void playIntro() {
         introTextTimeline.getKeyFrames().addAll(
-                new KeyFrame(Duration.seconds(1), e -> startTypewriterEffect("welcome to phone game.")),
-
-                new KeyFrame(Duration.seconds(4), e -> {
-                    startTypewriterEffect("this is your main display panel.");
+                new KeyFrame(Duration.seconds(1), e -> {
+                    dialogFlowManager.advanceState("0");
+                    startTypewriterEffect(dialogFlowManager.getGameStateMessage(), introTextTimeline);
+                }),
+                new KeyFrame(Duration.seconds(2), e -> {
+                    dialogFlowManager.advanceState("0");
+                    startTypewriterEffect(dialogFlowManager.getGameStateMessage(), introTextTimeline);
                     startBlinkAnimation(displayPane, R.speed.FAST, 6);
                 }),
-
-                new KeyFrame(Duration.seconds(7), e -> {
-                    System.out.println("7");
-                    startTypewriterEffect("this is where you'll reply to a conversation.");
+                new KeyFrame(Duration.seconds(3), e -> {
+                    dialogFlowManager.advanceState("0");
+                    startTypewriterEffect(dialogFlowManager.getGameStateMessage(), introTextTimeline);
                     startBlinkAnimation(replyPane, R.speed.FAST, 6);
                 }),
-
-                new KeyFrame(Duration.seconds(12), e -> {
-                    startTypewriterEffect("lets try it.");
-                    replyButton1.setText("nah, I don't trust you.");
-                    replyButton2.setText("okay let's begin!");
-                    startFadeAnimation(replyButton1, false, R.speed.NORMAL);
-                    startFadeAnimation(replyButton2, false, R.speed.NORMAL);
-                    replyButton1.setOnAction(actionEvent -> {
-                        dialogueCounter++;
-                        switch (dialogueCounter) {
-                            case 1:
-                                Timeline newTimeline = new Timeline(
-                                        new KeyFrame(Duration.seconds(0.1), g -> {
-                                            startTypewriterEffect("smart guy huh");
-                                            startFadeAnimation(replyButton1, true, R.speed.SLOW);
-                                            startFadeAnimation(replyButton2, true, R.speed.SLOW);
-                                        }),
-                                        new KeyFrame(Duration.seconds(2), f -> {
-                                            startTypewriterEffect("not so smart now are you?");
-                                            replyButton1.setText("i would like to apologize.");
-                                            startFadeAnimation(replyButton1, false, R.speed.SLOW);
-                                        })
-                                );
-                                newTimeline.play();
-                                break;
-
-                            case 2:
-                                startTypewriterEffect("123");
-                        }
-
-                    });
-
-                    replyButton2.setOnAction(actionEvent -> {
-                        introTextTimeline.getKeyFrames().add(
-                                new KeyFrame(Duration.seconds(12.1), f -> {
-                                    startTypewriterEffect("123");
-                                }));
-                    });
-
+                new KeyFrame(Duration.seconds(4), e -> {
+                    dialogFlowManager.advanceState("0");
+                    startTypewriterEffect(dialogFlowManager.getGameStateMessage(), introTextTimeline);
+                    showReplyButtons();
+                    String[] playerOptions = dialogFlowManager.getIntroPlayerOptions();
+                    replyButton1.setText(playerOptions.length > 0 ? playerOptions[0] : "");
+                    replyButton2.setText(playerOptions.length > 1 ? playerOptions[1] : "");
                     introTextTimeline.pause();
-                }));
+                }),
+                new KeyFrame(Duration.seconds(5), e -> {
+                    dialogFlowManager.advanceState("");
+                    startTypewriterEffect(dialogFlowManager.getGameStateMessage(), introTextTimeline);
+                })
+        );
         introTextTimeline.play();
     }
+
+    private void handlePlayerChoice(String theChoice) {
+        dialogFlowManager.advanceState(theChoice);
+
+    }
+
+    private void updateUI() {
+        startTypewriterEffect(dialogFlowManager.getGameStateMessage(), introTextTimeline);
+        String[] playerResponseOptions = dialogFlowManager.getIntroPlayerOptions();
+
+        if (playerResponseOptions.length > 0) {
+            replyButton1.setText(playerResponseOptions[0]);
+            startFadeAnimation(replyButton1, false, R.speed.NORMAL);
+            replyButton1.setOnAction(e -> {
+                handlePlayerChoice(playerResponseOptions[0]);
+            });
+        }
+        if (playerResponseOptions.length > 1) {
+            replyButton2.setText(playerResponseOptions[1]);
+            startFadeAnimation(replyButton2, false, R.speed.NORMAL);
+            replyButton2.setOnAction(e -> {
+                handlePlayerChoice(playerResponseOptions[1]);
+            });
+        }
+    }
+
+    public void showReplyButtons() {
+        startFadeAnimation(replyButton1, false, R.speed.NORMAL);
+        startFadeAnimation(replyButton2, false, R.speed.NORMAL);
+
+    }
+
+//    public void playIntro() {
+//        introTextTimeline.getKeyFrames().addAll(
+//                new KeyFrame(Duration.seconds(1), e -> startTypewriterEffect("welcome to phone game.")),
+//
+//                new KeyFrame(Duration.seconds(4), e -> {
+//                    startTypewriterEffect("this is your main display panel.");
+//                    startBlinkAnimation(displayPane, R.speed.FAST, 6);
+//                }),
+//
+//                new KeyFrame(Duration.seconds(7), e -> {
+//                    System.out.println("7");
+//                    startTypewriterEffect("this is where you'll reply to a conversation.");
+//                    startBlinkAnimation(replyPane, R.speed.FAST, 6);
+//                }),
+//
+//                new KeyFrame(Duration.seconds(12), e -> {
+//                    startTypewriterEffect("lets try it.");
+//                    replyButton1.setText("nah, I don't trust you.");
+//                    replyButton2.setText("okay let's begin!");
+//                    startFadeAnimation(replyButton1, false, R.speed.NORMAL);
+//                    startFadeAnimation(replyButton2, false, R.speed.NORMAL);
+//                    replyButton1.setOnAction(actionEvent -> {
+//                        dialogueCounter++;
+//                        switch (dialogueCounter) {
+//                            case 1:
+//                                Timeline newTimeline = new Timeline(
+//                                        new KeyFrame(Duration.seconds(0.1), g -> {
+//                                            startTypewriterEffect("smart guy huh");
+//                                            startFadeAnimation(replyButton1, true, R.speed.SLOW);
+//                                            startFadeAnimation(replyButton2, true, R.speed.SLOW);
+//                                        }),
+//                                        new KeyFrame(Duration.seconds(2), f -> {
+//                                            startTypewriterEffect("not so smart now are you?");
+//                                            replyButton1.setText("i would like to apologize.");
+//                                            startFadeAnimation(replyButton1, false, R.speed.SLOW);
+//                                        })
+//                                );
+//                                newTimeline.play();
+//                                break;
+//
+//                            case 2:
+//                                startTypewriterEffect("123");
+//                        }
+//
+//                    });
+//
+//                    replyButton2.setOnAction(actionEvent -> {
+//                        introTextTimeline.getKeyFrames().add(
+//                                new KeyFrame(Duration.seconds(12.1), f -> {
+//                                    startTypewriterEffect("123");
+//                                }));
+//                    });
+//
+//                    introTextTimeline.pause();
+//                }));
+//        introTextTimeline.play();
+//    }
 
 
 }
--- src/main/java/res/R.java
+++ src/main/java/res/R.java
@@ -14,4 +14,19 @@ public class R {
         public static final double INVISIBLE = 0.0;
         public static final double VISIBLE = 1.0;
     }
+
+    public static class introMessages {
+        public static final String WELCOME_MSG = "welcome to phone game.";
+        public static final String DISPLAY_PANEL_MSG = "this is your main display panel.";
+        public static final String REPLY_CONVO_MSG = "this is where you'll reply to conversations.";
+        public static final String TRY_BUTTON_MSG = "let's try it.";
+        public static final String SMART_GUY_MSG = "smart guy huh";
+        public static final String NOT_SMART_MSG = "not so smart now are you?";
+    }
+
+    public static class introPlayerOptions {
+        public static final String OPTIONS_1A = "nah, I don't trust you.";
+        public static final String OPTIONS_1B = "Okay, let's begin!";
+        public static final String OPTIONS_2 = "i would like to apologize.";
+    }
 }
